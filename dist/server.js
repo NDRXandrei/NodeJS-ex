7f3b8bcf53139bf06410d2f2e4aa2e630b794c28
Andrei <ndrx544@gmail.com>
2023-01-07 17:02:08 +0100

    fatto server, il test però da qualche problema, ma nel complesso funziona

--- dist/server.js
+++ dist/server.js
@@ -3,15 +3,10 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = __importDefault(require("express"));
-require("express-async-errors");
-const app = (0, express_1.default)();
-const message = "Server On";
-app.get("/", (request, response) => {
-    response.send(message);
-});
-const port = 3000;
-app.listen(port, () => {
+require("dotenv/config");
+const app_1 = __importDefault(require("./app"));
+const port = process.env.PORT;
+app_1.default.listen(port, () => {
     console.log(`Server is running at http://localhost:${port}`);
 });
 //# sourceMappingURL=server.js.map
--- src/server.ts
+++ src/server.ts
@@ -1,15 +1,7 @@
-import express from "express";
-import "express-async-errors";
+import "dotenv/config";
+import app from "./app";
 
-const app = express();
-
-const message = "Server On";
-
-app.get("/", (request, response) => {
-  response.send(message);
-});
-
-const port = 3000;
+const port = process.env.PORT;
 
 app.listen(port, () => {
   console.log(`Server is running at http://localhost:${port}`);
